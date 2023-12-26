76d3a308c39506d72b36232361f8a1c357f0c9bc
Vincent Mayer <vincent.mayer@tum.de>
2023-12-26 15:06:51 +0100

    ref

--- gemm/src/kernels/04_1d_blocktiling.cuh
+++ gemm/src/kernels/04_1d_blocktiling.cuh
@@ -9,28 +9,29 @@ __global__ void blocktiling1DGemmKernel(int N, const float *A, const float *B, f
     uint grow = blockIdx.y * blockDim.y + threadIdx.y * RESULTS_PER_THREAD;
     uint gcol = blockIdx.x * blockDim.x + threadIdx.x;
 
-    __shared__ float As[TILESIZE][TILESIZE / RESULTS_PER_THREAD];
-    __shared__ float Bs[TILESIZE / RESULTS_PER_THREAD][TILESIZE];
+    const uint tileSizeY = TILESIZE / RESULTS_PER_THREAD;
+
+    __shared__ float As[TILESIZE][tileSizeY];
+    __shared__ float Bs[tileSizeY][TILESIZE];
 
     float threadResults[RESULTS_PER_THREAD] = {0.0};
-    const uint nTilesCommon = N / (TILESIZE / RESULTS_PER_THREAD);
+    const uint nTilesCommon = N / (tileSizeY);
 
     for (int tileIdx = 0; tileIdx < nTilesCommon; ++tileIdx)
     {
         for (int i = 0; i < RESULTS_PER_THREAD; ++i)
         {
             As[threadIdx.y + i][threadIdx.x] =
-                A[(grow + i) * N + tileIdx * (TILESIZE / RESULTS_PER_THREAD) +
-                  threadIdx.x];
+                A[(grow + i) * N + tileIdx * (tileSizeY) + threadIdx.x];
         }
         Bs[threadIdx.y][threadIdx.x] =
-            B[gcol + tileIdx * N * (TILESIZE / RESULTS_PER_THREAD) + threadIdx.y * N];
+            B[gcol + tileIdx * N * (tileSizeY) + threadIdx.y * N];
 
         __syncthreads();
 
         // Perform work in the tile leveraging data in shared mem
         // Loop over the dot product dimension of the tile (common dimension)
-        for (int i = 0; i < TILESIZE / RESULTS_PER_THREAD; ++i)
+        for (int i = 0; i < tileSizeY; ++i)
         {
             float tmpB = Bs[i][threadIdx.x];
             // Additionally compute multiple outputs
